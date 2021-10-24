ca8129cf41e22840994e50189a03081e3b7ddd6a
Cristóbal <cristobal_jjg@hotmail.com>
2021-10-24 12:44:31 +0100

    Money_Calculator; Version 1; GI+Modelo

--- src/money_calculator/Money_Calculator.java
+++ src/money_calculator/Money_Calculator.java
@@ -1,10 +1,13 @@
 package money_calculator;
 
 import Interfaz.*;
+import Modelo.*;
 
 public class Money_Calculator {
 
     public static void main(String[] args){
-        View i = new View();
+        //View i = new View();
+        Modelo m = new Modelo(1, "BTC", "EUR");
+        m.calculateExchange();
     }
 }
